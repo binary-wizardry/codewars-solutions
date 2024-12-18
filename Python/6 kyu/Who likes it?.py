797bfb52f5d0a37c5a29f64d328ad1ee974e0f23
Anton <efimov.10.111@gmail.com>
2024-12-18 13:52:14 +0300

    Kata added

---
+++ Python/6 kyu/Who likes it?.py
@@ -0,0 +1,15 @@
+# Who likes it?
+# https://www.codewars.com/kata/5266876b8f4bf2da9b000362
+
+def likes(names):
+    match names:
+        case []:
+            return "no one likes this"
+        case [a]:
+            return f"{a} likes this"
+        case [a, b]:
+            return f"{a} and {b} like this"
+        case [a, b, c]:
+            return f"{a}, {b} and {c} like this"
+        case [a, b, *rest]:
+            return f"{a}, {b} and {len(rest)} others like this"
