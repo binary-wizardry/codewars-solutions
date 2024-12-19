c47ea366350a952f721cfb744154fa01c1c3cf10
Anton <efimov.10.111@gmail.com>
2024-12-19 16:39:12 +0300

    Kata added

---
+++ Python/6 kyu/Does my number look big in this?.py
@@ -0,0 +1,5 @@
+# Does my number look big in this?
+# https://www.codewars.com/kata/5287e858c6b5a9678200083c
+
+def narcissistic( value ):
+    return sum(int(n) ** len(str(value)) for n in str(value)) == value
