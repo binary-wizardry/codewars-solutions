997e8a4d048a5323c90ce4ba653697a5c0d02128
Anton <efimov.10.111@gmail.com>
2024-12-21 13:59:49 +0300

    Kata added

---
+++ Python/5 kyu/First non-repeating character.py
@@ -0,0 +1,8 @@
+# First non-repeating character
+# https://www.codewars.com/kata/52bc74d4ac05d0945d00054e
+
+def first_non_repeating_letter(s):
+    for c in s:
+        if s.lower().count(c.lower()) == 1:
+            return c
+    return ''
